eaf05c6485d2c12c34345cd5f834194c3e738f13
amtoha-prog <amtoha@alustudent.com>
2026-07-04 07:40:16 -0700

    Correct

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#Finds all multiples of 2 in a list
+# Finds all multiples of 2 in a list
 
 
 def divisible_by_2(my_list=[]):
-   # Returns list of True/False for divisibility by 2
     return [i % 2 == 0 for i in my_list]
