86fad20fc2716d123e999caf7f82b652ae58cacc
amtoha-prog <amtoha@alustudent.com>
2026-07-04 05:59:19 -0700

    Secure access

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 # Access to an element
 
 def element_at(my_list, idx):
-    if idx < 0 or  idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
