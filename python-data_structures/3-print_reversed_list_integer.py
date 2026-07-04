c2613dab6a32de92dc3524612057319d6b7c1341
amtoha-prog <amtoha@alustudent.com>
2026-07-04 06:38:30 -0700

    Replace

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Prints all integers of a list in reverse order
+# Prints all integers of a list in reverse order
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
