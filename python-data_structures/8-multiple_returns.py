09abce94dcce842166d9a9e0c85969c226471bda
amtoha-prog <amtoha@alustudent.com>
2026-07-06 03:01:14 -0700

    More returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#Returns length and first character of a string
+
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
