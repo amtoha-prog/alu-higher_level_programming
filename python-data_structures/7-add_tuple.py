749b276f201bb85f87a2eea40fe59f391ac7ebe0
amtoha-prog <amtoha@alustudent.com>
2026-07-06 03:05:55 -0700

    tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
-e
+#!/usr/bin/python3
+# Adds 2 tuples
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    #Returns tuple with sum of first 2 elements
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (a1 + b1, a2 + b2)
